9c6d6b4bd3f5db878d4bb9fb11e5d8fab989ad9c
pushpak khunt <87639656+pushpakkhunt@users.noreply.github.com>
2026-04-14 20:23:39 -0700

    Changed in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,17 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        app: resolve(__dirname, 'src/app.html'),
+      },
+    },
   },
   server: {
     port: 5173,
